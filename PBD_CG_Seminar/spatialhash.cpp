cd7d95db82148b1a12ed6fbeecd593437bd39994
Wladimir Silantiy Leuschner <wladimir.s.leuschner@campus.tu-berlin.de>
2016-01-04 14:17:40 +0100

    Normalmatrix now calculated in CPU

---
+++ PBD_CG_Seminar/spatialhash.cpp
@@ -0,0 +1,5 @@
+#include "spatialhash.h"
+
+SpatialHash::SpatialHash()
+{
+}
---
+++ PBD_CG_Seminar/spatialhash.h
@@ -0,0 +1,10 @@
+#ifndef SPATIALHASH_H
+#define SPATIALHASH_H
+
+class SpatialHash
+{
+public:
+    SpatialHash();
+};
+
+#endif // SPATIALHASH_H
